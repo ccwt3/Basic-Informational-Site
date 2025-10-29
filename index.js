c0e5a9ae284f3a506f5c497c8ed190b1b3bf4f73
MoyyyL <moyyyl0526@gmail.com>
2025-10-28 18:53:47 -0600

    Another itteration now it uses asyncronous functions to read de html

--- index.js
+++ index.js
@@ -2,27 +2,59 @@ import http from 'node:http';
 import fs from 'node:fs';
 
 const server = http.createServer();
-
-let data;
+const directory = {
+  index: 'C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/index.html',
+  about: 'C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/about.html',
+  contact: 'C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/contact-me.html',
+  notFound: 'C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/404.html',
+}
 
 server.on('request', (request, res) => {
   res.writeHead(200, { 'content-type': 'text/html' });
-
-  if (request.url === '/' || request.url === '/favicon.ico') {
-    data = fs.readFileSync('C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/index.html', 'utf8');
+  if (request.url === '/') {
+    fs.readFile(directory.index, 'utf-8', (err, data) => {    
+      if (err) {
+        res.end();
+        return 500;
+      } else {
+        res.write(data);
+        res.end()
+      }
+    })
   }
   else if (request.url === '/about') {
-    data = fs.readFileSync('C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/about.html', 'utf-8');
+    fs.readFile(directory.about, 'utf-8', (err, data) => {    
+      if (err) {
+        res.end();
+        return 500;
+      } else {
+        res.write(data);
+        res.end()
+      }
+    })
   }
   else if (request.url === '/contact-me') {
-    data = fs.readFileSync('C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/contact-me.html', 'utf-8');
+    fs.readFile(directory.contact, 'utf-8', (err, data) => {    
+      if (err) {
+        res.end();
+        return 500;
+      } else {
+        res.write(data);
+        res.end()
+      }
+    })
   }
   else {
-    data = fs.readFileSync('C:/Users/cacaw/Documents/Visual_studio/Fullstack-Course/NODE JS/Basic Project/404.html', 'utf-8');
+    fs.readFile(directory.notFound, 'utf-8', (err, data) => {    
+      if (err) {
+        res.end();
+        return 500;
+      } else {
+        res.write(data);
+        res.end()
+      }
+    })
   }
-
-  res.write(data);
-  res.end();
 })
 
 server.listen(8080);
